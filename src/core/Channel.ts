35d029c262b3ef8cdc85eb7e3b3c62ae05a54fb5
amankukreti90 <kukretiaman635@gmail.com>
2026-09-14 11:33:16 +0530

    Update Channel.ts

--- src/core/Channel.ts
+++ src/core/Channel.ts
@@ -1,10 +1,29 @@
 class Channel {
+    static #count: number = 1000;
 
+    #id: number;
+    #name: string;
+    #createdBy: number;
 
+    constructor(name: string, createdBy: number) {
+        Channel.#count++;
 
+        this.#id = Channel.#count;
+        this.#name = name;
+        this.#createdBy = createdBy;
+    }
 
-  
+    getId(): number {
+        return this.#id;
+    }
 
+    getName(): string {
+        return this.#name;
+    }
+
+    getCreatedBy(): number {
+        return this.#createdBy;
+    }
 }
 
 export default Channel;
