ba02d14fc8d0df4db2cd08c1b2be163e59fb7cff
amankukreti90 <kukretiaman635@gmail.com>
2026-09-14 12:39:36 +0530

    Update Channel.ts
    added group and message class for future implementation.

--- src/core/Channel.ts
+++ src/core/Channel.ts
@@ -1,16 +1,56 @@
-class Channel {
+import User from './User';
+
+
+class Message {
+    static #count: number = 1000;
+
+    #id: number;
+    #content: string;
+    #sender: User;
+    #timestamp: Date;
+
+    constructor(content: string, sender: User) {
+        Message.#count++;
+
+        this.#id = Message.#count;
+        this.#content = content;
+        this.#sender = sender;
+        this.#timestamp = new Date();
+    }
+
+    getId(): number {
+        return this.#id;
+    }
+
+    getContent(): string {
+        return this.#content;
+    }
+
+    getSender(): User {
+        return this.#sender;
+    }
+
+    getTimestamp(): Date {
+        return this.#timestamp;
+    }
+}
+
+
+class Group {
     static #count: number = 1000;
 
     #id: number;
     #name: string;
-    #createdBy: number;
+    #users: User[];
+    #messages: Message[];
 
-    constructor(name: string, createdBy: number) {
-        Channel.#count++;
+    constructor(name: string) {
+        Group.#count++;
 
-        this.#id = Channel.#count;
+        this.#id = Group.#count;
         this.#name = name;
-        this.#createdBy = createdBy;
+        this.#users = [];
+        this.#messages = [];
     }
 
     getId(): number {
@@ -21,9 +61,74 @@ class Channel {
         return this.#name;
     }
 
-    getCreatedBy(): number {
-        return this.#createdBy;
+    getUsers(): User[] {
+        return this.#users;
+    }
+
+    getMessages(): Message[] {
+        return this.#messages;
+    }
+
+    addUser(user: User): void {
+        this.#users.push(user);
+    }
+
+    removeUser(user: User): void {
+        this.#users = this.#users.filter(
+            (u) => u.getId() !== user.getId()
+        );
+    }
+
+    addMessage(message: Message): void {
+        this.#messages.push(message);
+    }
+
+    removeMessage(message: Message): void {
+        this.#messages = this.#messages.filter(
+            (m) => m.getId() !== message.getId()
+        );
     }
 }
 
+
+class Channel {
+    static #count: number = 1000;
+
+    #id: number;
+    #name: string;
+    #groups: Group[];
+
+    constructor(name: string) {
+        Channel.#count++;
+
+        this.#id = Channel.#count;
+        this.#name = name;
+        this.#groups = [];
+    }
+
+    getId(): number {
+        return this.#id;
+    }
+
+    getName(): string {
+        return this.#name;
+    }
+
+    getGroups(): Group[] {
+        return this.#groups;
+    }
+
+    addGroup(group: Group): void {
+        this.#groups.push(group);
+    }
+
+    removeGroup(group: Group): void {
+        this.#groups = this.#groups.filter(
+            (g) => g.getId() !== group.getId()
+        );
+    }
+}
+
+
+export { Group, Message };
 export default Channel;
